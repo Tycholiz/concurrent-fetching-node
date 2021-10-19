cadd728c779436f0818bc349e20a28ccea1111c3
Kyle Tycholiz <39745457+Tycholiz@users.noreply.github.com>
2021-10-19 13:27:41 -0700

    pass query params to api/posts route

--- @app/controllers/ping.ts
+++
@@ -1,9 +0,0 @@
-class Controller {
-    async ping() {
-        return null
-    }
-
-    async getPosts() {
-        return null
-    }
-}
--- @app/routes/index.ts
+++ @app/routes/index.ts
@@ -3,7 +3,17 @@ import { Application, Request, Response } from "express";
 export const register = (app: Application) => {
     app.get('/api/ping', (req: Request, res: Response) => {
         console.log('hey!');
+
+        res.status(200).json({
+            "success": true
+        })
+    })
+    app.get('/api/posts', (req: Request, res: Response) => {
+        const { tags, sortBy, direction } = req.query
+        console.log(tags);
         
-        res.sendStatus(200)
-    })    
+
+        
+        const url = `https://api.hatchways.io/assessment/blog/posts?tag=${tags}`
+    })
 }
