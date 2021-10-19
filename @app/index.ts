95dab36fe65c348957e7824c042bf036ff30ad1f
Kyle Tycholiz <39745457+Tycholiz@users.noreply.github.com>
2021-10-19 12:43:57 -0700

    initial commit

---
+++ @app/index.ts
@@ -0,0 +1,10 @@
+const http = require('http')
+const express = require('express')
+
+
+const PORT = process.env.PORT || 5000;
+const app = express()
+
+app.listen(PORT, () => {
+    console.log(`server started on port: ${PORT}`);
+});
