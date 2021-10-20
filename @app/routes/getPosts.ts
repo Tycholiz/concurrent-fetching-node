396ade0d344b846ab97c233187f72fb8d5af7110
Kyle Tycholiz <39745457+Tycholiz@users.noreply.github.com>
2021-10-19 20:30:00 -0700

    brush tsc issue under the rug

--- @app/routes/getPosts.ts
+++ @app/routes/getPosts.ts
@@ -3,7 +3,7 @@ import { AscendingDescending, Post, SortByOptions } from '../global'
 import { arrayToObject } from '../utils'
 
 
-export const getPosts = async (tagList: string[], sortBy: SortByOptions = 'id', direction: AscendingDescending = 'asc'): Promise<Post[]> => {
+export const getPosts = async (tagList: string[], sortBy: SortByOptions = 'id', direction: AscendingDescending = 'asc'): Promise<Post[] | unknown[]> => {
     const requests = tagList.map(tag => axios.get(`https://api.hatchways.io/assessment/blog/posts?tag=${tag}`))
 
     const responses = await Promise.all(requests)
@@ -13,5 +13,5 @@ export const getPosts = async (tagList: string[], sortBy: SortByOptions = 'id', 
     /* Transform to array to remove duplicate pairs */
     const allDataAsObj = arrayToObject(dataFromAllQueries.flat(), sortBy)
     
-    return direction === 'desc' ? Object.values(allDataAsObj).reverse() as Post[] : Object.values(allDataAsObj)
+    return direction === 'desc' ? Object.values(allDataAsObj).reverse() : Object.values(allDataAsObj)
 }
